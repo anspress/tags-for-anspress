21edbc06803e9a3ce037e5961da023f2efe04f83
shadowshades <gagnon.shad@hotmail.com>
2015-09-23 15:51:43 -0400

    Rebuild + Keyboard navigation + Accessibility + JS Translation
    The process has been improved and some bugs have been fixed.
    Keyboard navigation is now available.
    Accessibility features was added for the blind people using screen
    reader.
    Translation is now available inside tags_js.js.
    Some CSS improvements.

--- theme/js/tags_js.js
+++ theme/js/tags_js.js
@@ -20,34 +20,62 @@
 	    return str;
 	}
 
-	function apAddTag(str, append){
+	function apAddTag(str, container){
 		str = str.replace(/,/g, '');
 		str = str.trim();
 		str = apSanitizeTitle(str);
-
+		
 		if( str.length > 0 ){
-			var html = $('<span class="ap-tagssugg-item" style="display:none">'+str+'<i class="ap-tag-remove">×</i><input type="hidden" name="tags[]" value="'+str+'" /></span>');
 
-			if(typeof append === 'undefined'){
-				var exsists = false;
-				var exsist_el = false;
-				$('#ap-tags-holder .ap-tagssugg-item').each(function(index, el) {
-					if(apSanitizeTitle($(this).text()) == str){
-						exsists = true;
-						exsist_el = $(this);
-					}
-				});
-
-				if(exsists){
-					$(exsist_el).animate({opacity: 0}, 100, function(){
-						$(this).animate({opacity: 1}, 400);
-					});
-				}else{
-					html.appendTo('#ap-tags-holder').fadeIn(300);
-				}
-			}else{
-				return html;
+			var htmlTag = {
+				element : 'li',
+				class : 'ap-tagssugg-item',
+				itemValueClass : 'ap-tag-item-value',
+				button : {
+					class : 'ap-tag-add',
+					icon : 'apicon-plus',
+				},
+				input : '',
+				accessibilityText : apTagsTranslation.addTag
 			}
+			
+			// Add tag to the main container (holder list), 
+			// Else add tag to a specific container (suggestion list)
+			if(!container){
+				
+				var container = '#ap-tags-holder';
+				htmlTag.button.class = 'ap-tag-remove';
+				htmlTag.button.icon = 'apicon-x';
+				htmlTag.input = '<input type="hidden" name="tags[]" value="'+str+'" />';
+				htmlTag.accessibilityText = apTagsTranslation.deleteTag;
+				
+				var exist_el = false;
+				$(container).find('.'+htmlTag.class).find('.'+htmlTag.itemValueClass).each(function(index, el) {
+					if(apSanitizeTitle($(this).text()) == str)
+						exist_el = $(this);
+				});
+				if (exist_el !== false) { // If the element already exist, stop and dont add tag
+					exist_el.animate({opacity: 0}, 100, function(){
+						exist_el.animate({opacity: 1}, 400);
+					});
+					return; 
+				}
+				
+				if (!$('#tags').is(':focus'))
+					$('#tags').val('').focus();
+					
+				$('#ap-tags-suggestion').hide();
+				
+				// Message for screen reader
+				// Timeout used to resolve a bug with JAWS and IE...
+				setTimeout(function() {
+					$('#ap-tags-aria-message').text(str + " " + apTagsTranslation.tagAdded);
+				}, 250);
+			}
+			
+			var html = $('<'+htmlTag.element+' class="'+htmlTag.class+'" title="'+htmlTag.accessibilityText+'"><button role="button" class="'+htmlTag.button.class+'"><span class="sr-only">'+htmlTag.accessibilityText+'"</span> <span class="'+htmlTag.itemValueClass+'">'+str+'</span><i class="'+htmlTag.button.icon+'"></i></button>'+htmlTag.input+'</'+htmlTag.element+'>');
+			html.appendTo(container).fadeIn(300);
+			
 		}
 	}
 
@@ -66,34 +94,39 @@
 			dataType:'json',
 			success: function(data){
 				AnsPress.site.hideLoading(this);
+				
 				console.log(data);
+				
+				$('#ap-tags-suggestion').html('');
+				
 				if(!data.status)
 					return;
 
-				var container = jQuery(this).closest('.bootstrap-tagsinput'),
-					position = container.offset();
-
-				$('#ap-tags-suggestion').html('');
-
+				if (!$('#ap-tags-suggestion').is(':visible')) {
+					$('#ap-tags-suggestion').show();
+				}
+						
 				if(data['items']){
 					$.each(data['items'], function(index, val) {
-						var html = apAddTag(val, false);
-						$('#ap-tags-suggestion').append($(html).fadeIn(300));
+						var holderItems = [];
+						$("#ap-tags-holder .ap-tag-item-value").each(function() {
+							holderItems.push($(this).text())
+						});
+						if ($.inArray(val, holderItems)<0) // Show items that was not already inside the holder list
+							apAddTag(val, '#ap-tags-suggestion');
 					});
 				}
-
+				
+				// Message for screen reader
+				// Timeout used to resolve a bug with JAWS and IE...
+				setTimeout(function() {
+					$('#ap-tags-aria-message').text(apTagsTranslation.suggestionsAvailable);
+				}, 250);
 			}
 		});
 	}
 
 	$(document).ready(function(){
-
-		$(window).keydown(function(event){
-			if( event.keyCode == 13 && $(event.target).is('#ap-tags-input') ) {
-				event.preventDefault();
-				return false;
-			}
-		});
 
 		$('#tags').on('apAddNewTag',function(e){
 			e.preventDefault();
@@ -101,33 +134,90 @@
 			$(this).val('');
 		});
 
-		$('#tags').keyup(function(e){
-			e.preventDefault();
-			if(e.keyCode == 13 || e.keyCode == 188 )
-			{
-				clearTimeout(window.tagtime);
-				$(this).trigger('apAddNewTag');
+		$('#tags').on('keydown', function(e) {
+			if(e.keyCode == 13) { // Prevent submit form on Enter
+			  	e.preventDefault();
+			  	return false;
+			}
+			if(e.keyCode == 38 || e.keyCode == 40) {
+				var inputs = $('#ap-tags-suggestion').find('.ap-tag-add');
+				var focused = $('#ap-tags-suggestion').find('.focus');
+				var index = inputs.index(focused);
+				
+				if(index != -1) {
+					if(e.keyCode == 38) // up arrow
+						index--;
+					if(e.keyCode == 40) // down arrow
+						index++;
+				}
+				else {
+					if(e.keyCode == 38) // up arrow
+						index = inputs.length-1;
+					if(e.keyCode == 40) // down arrow
+						index = 0;
+				}
+				
+				if (index >= inputs.length)
+					index = -1;
+				
+				inputs.removeClass('focus');
+				
+				if(index != -1) {
+					inputs.eq(index).addClass('focus');
+					$(this).val(inputs.eq(index).find('.ap-tag-item-value').text());
+				} 
+				else {
+					$(this).val($(this).attr('data-original-value'));
+				}
 			}
 		});
 
-		$('body').delegate('.ap-tagssugg-item', 'click', function(event) {
-			$(this).remove();
-		});
-
-		$('#tags').keypress(function(e) {
+		$('#tags').on('keyup focus', function(e) {
+			e.preventDefault();
 			var val = $(this).val().trim();
 			clearTimeout(window.tagtime);
-			window.tagtime = setTimeout(function() {
-				if(val.length > 1){
-					apTagsSuggestion(val);
+			if(val.length > 1) {
+				if(e.keyCode != 9 && e.keyCode != 37 && e.keyCode != 38 && e.keyCode != 39 && e.keyCode != 40) { // Do nothing on Tab and arrows keys
+					if(e.keyCode == 13 || e.keyCode == 188 ) { // "Enter" or ","
+						clearTimeout(window.tagtime);
+						$(this).trigger('apAddNewTag');
+					} else {
+						$(this).attr('data-original-value', $(this).val());
+						window.tagtime = setTimeout(function() {
+							apTagsSuggestion(val);
+						}, 200);
+					}
 				}
-			}, 200);
+			} else {
+				$('#ap-tags-suggestion').hide();
+			}
 		});
-
-		$('#ap-tags-suggestion').delegate('span', 'click', function(e) {
-			apAddTag($(this).text());
+		
+		$('#ap-tags-suggestion').delegate('.ap-tagssugg-item', 'click', function(e) {
+			apAddTag($(this).find('.ap-tag-item-value').text());
 			$(this).remove();
 		});
+
+		$('body').on('click focusin', function(e) {
+			if ($('#ap-tags-suggestion').is(':visible') && $(e.target).parents('#ap-tags-add').length <= 0)
+			  	$('#ap-tags-suggestion').hide();
+		});
+		
+		$('body').delegate('.ap-tagssugg-item', 'click', function(event) {
+			var itemValue = $(this).find('.ap-tag-item-value').text();
+			
+			// Message for screen reader
+			// Timeout used to resolve a bug with JAWS and IE...
+			setTimeout(function() {
+				$('#ap-tags-aria-message').text(itemValue + " " + apTagsTranslation.tagRemoved);
+			}, 250);
+			
+			$(this).remove();
+			$('#ap-tags-list-title').focus();
+		});
+		
+		// Message used by screen reader to get suggestions list or a confirmation when a tag is added
+		$('body').append('<div role="status" id="ap-tags-aria-message" aria-live="polite" aria-atomic="true" class="sr-only"></div>');
 	})
 
 })(jQuery)
