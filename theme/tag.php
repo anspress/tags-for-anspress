8044fcf7b6f116ef544f4aa54f0b12908d9d09ff
rahularyan <rah12@live.com>
2016-02-05 11:03:54 +0530

    Fix: updated text domain

--- theme/tag.php
+++ theme/tag.php
@@ -9,7 +9,7 @@
 <div class="row">
 	<div id="ap-lists" class="<?php echo is_active_sidebar( 'ap-tag' ) && is_anspress() ? 'col-md-9' : 'col-md-12' ?>">
 		<div class="ap-taxo-detail">
-			<h2 class="entry-title"><?php printf(__('Question tag: %s','ap'), $question_tag->name); ?> <span class="ap-tax-item-count"><?php printf( _n('1 Question', '%s Questions', $question_tag->count, 'ap'),  $question_tag->count); ?></span></h2>
+			<h2 class="entry-title"><?php printf(__('Question tag: %s','anspress-question-answer'), $question_tag->name); ?> <span class="ap-tax-item-count"><?php printf( _n('1 Question', '%s Questions', $question_tag->count, 'anspress-question-answer'),  $question_tag->count); ?></span></h2>
 			<?php if($question_tag->description !=''): ?>
 				<p class="ap-taxo-description"><?php echo $question_tag->description; ?></p>
 			<?php endif; ?>
